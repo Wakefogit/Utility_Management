8afcbf1f4ece162d371b608115e756d3bbdbeebb
Noor Mohammed <noor.mohammed@wakeflo.com>
2023-09-20 18:30:31 +0530

    updated noor

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -113,7 +113,7 @@ const routes = [
 
 
           icon: (
-<svg xmlns="http://www.w3.org/2000/svg"
+         <svg xmlns="http://www.w3.org/2000/svg"
           width="24" height="24" 
           viewBox="0 0 24 24">
           <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
