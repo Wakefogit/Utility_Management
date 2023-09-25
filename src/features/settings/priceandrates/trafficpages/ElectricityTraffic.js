fc7a408aa198ee43544353554a50ebbdf8b36160
Anandkumar B <anandbalu324@gmail.com>
2023-09-25 16:37:57 +0530

    added validation

--- src/features/settings/priceandrates/trafficpages/ElectricityTraffic.js
+++ src/features/settings/priceandrates/trafficpages/ElectricityTraffic.js
@@ -7,6 +7,14 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import { useNavigate } from 'react-router-dom';
 
 const style = {
   position: "absolute",
@@ -19,15 +27,52 @@ const style = {
   boxShadow: 10,
   p: 4,
 };
+const validationSchema = Yup.object().shape({
+  validFrom: Yup.date().required("Valid from is required"),
+  currency: Yup.string().required("Currency is required"),
+  standingCharge: Yup.number().required("Standing charge is required"),
+  basicTariffRate: Yup.number().required("Basic tariff rate is required"),
+});
 
+const initialValues = {
+  validFrom: "",
+  validTo: "",
+  currency: "",
+  standingCharge: "",
+  basicTariffRate: "",
+  lowTariffRate: "",
+  note: "",
+};
 const ElectricityTraffic = () => {
   const dispatch = useDispatch();
+const navigate = useNavigate()
 
   useEffect(() => {
     dispatch(setPageTitle({ title: "Electricity  Tariff" }));
   }, []);
 
-  const [selectedCurrencySymbol, setSelectedCurrencySymbol] = useState("");
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      // Handle form submission and send data to the backend here
+      console.log(values);
+
+      if (formik.isValid) {
+        // Redirect to /app/Price
+        navigate("/app/Price");
+    }
+  }
+
+  });
+
+  // const [validFrom, setValidFrom] = useState("");
+  // const [validTo, setValidTo] = useState("");
+  // const [selectedCurrencySymbol, setSelectedCurrencySymbol] = useState("");
+  // const [standingCharge, setStandingCharge] = useState("");
+  // const [basicTariffRate, setBasicTariffRate] = useState("");
+  // const [lowTariffRate, setLowTariffRate] = useState("");
+  // const [note, setNote] = useState("");
 
   // Create separate state variables for each modal
   const [standingChargeModalOpen, setStandingChargeModalOpen] = useState(false);
@@ -43,217 +88,230 @@ const ElectricityTraffic = () => {
   const handleLowTariffOpen = () => setLowTariffModalOpen(true);
   const handleLowTariffClose = () => setLowTariffModalOpen(false);
 
-// Event handler for currency selection
-const handleCurrencyChange = (e) => { 
-  console.log(e)
-  const selectedValue = e.target.value;
-  console.log("Selected Currency:", selectedValue);
-  setSelectedCurrencySymbol(selectedValue);
-};
+  // const sendDataToBackend = async () => {
+  //   let apiUrl = "http://192.168.0.104:8080/createelectricity";
+  //   const response = await axios.post(
+  //     apiUrl,
+  //     {
+  //       fromDate: validFrom,
+  //       toDate: validTo,
+  //       currency: selectedCurrencySymbol,
+  //       charge: standingCharge,
+  //       basicPrice: basicTariffRate,
+  //       lowPrice: lowTariffRate,
+  //       note: note,
+  //     },
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  // };
 
   return (
-    <>
-      {/* Rest of your JSX code */}
+    <form onSubmit={formik.handleSubmit} className="m-8 space-y-4">
       <div>
         <label
-          htmlFor="countries"
-          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white "
+          htmlFor="validFrom"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Valid from <span className="text-red-500">*</span>
         </label>
-        <input
+        <TextField
+          sx={{
+            width: "75%",
+            "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+              height: "0.8375em",
+            },
+          }}
+          id="validFrom"
+          name="validFrom"
           type="date"
-          id="date"
-          className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={formik.values.validFrom}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.validFrom && Boolean(formik.errors.validFrom)}
+          helperText={formik.touched.validFrom && formik.errors.validFrom}
         />
       </div>
-
       <div>
         <label
-          htmlFor="countries"
-          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white "
+          htmlFor="validTo"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Valid to
         </label>
-        <input
+        <TextField
+          sx={{
+            width: "75%",
+            "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+              height: "0.8375em",
+            },
+          }}
+          id="validTo"
+          name="validTo"
           type="date"
-          id="date"
-          className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={formik.values.validTo}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.validTo && Boolean(formik.errors.validTo)}
+          helperText={formik.touched.validTo && formik.errors.validTo}
         />
       </div>
 
       <div>
-        <label         
-          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white "                
+        <label
+          htmlFor="currency"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Currency <span className="text-red-500">*</span>
         </label>
-        <select
-          id="countries"
-          className="bg-blue-50 mb-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[75%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={handleCurrencyChange}
-          placeholder="choose a currency"
-          // defaultValue={}
+        <Select
+          sx={{ width: "75%", height: "45px" }}
+          id="currency"
+          name="currency"
+          value={formik.values.currency}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.currency && Boolean(formik.errors.currency)}
         >
-          <option defaultValue="" >Choose a Currency</option>
-          <option value="₹">Indian Rupees (₹)</option>
-          <option value="$">United States Dollar ($)</option>
-          <option value="€">Euro (€)</option>
-          <option value="FC">Congolese franc (FC)</option>
-        </select>
-        
+          <MenuItem value="">Choose a Currency</MenuItem>
+          <MenuItem value="₹">Indian Rupees (₹)</MenuItem>
+          <MenuItem value="$">United States Dollar ($)</MenuItem>
+          <MenuItem value="€">Euro (€)</MenuItem>
+          <MenuItem value="FC">Congolese franc (FC)</MenuItem>
+        </Select>
       </div>
 
-      <div className="mb-6">
+      <div>
         <label
-          htmlFor="standing-rate"
-          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
+          htmlFor="standingCharge"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Standing charge <span className="text-red-500">*</span>
         </label>
-        <div className="flex items-center">
-          <input
-            type="text"
-            id="standing-rate"
-            className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <span className="text-sm text-gray-500 ml-2">
-          {selectedCurrencySymbol} / month{" "}
-          </span>
-          <div>
-            <HelpOutlinedIcon
-              onClick={handleStandingChargeOpen}
-              className="ml-3"
-            ></HelpOutlinedIcon>
-            <Modal
-              open={standingChargeModalOpen} // Use the correct state variable
-              onClose={handleStandingChargeClose} // Use the correct close handler
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Standing charge
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Enter the value of your monthly fees for electricity. If you
-                  aren't sure about it, check the costs in your last electricity
-                  invoice or in the pricelist of your electricity retailer.
-                </Typography>
-              </Box>
-            </Modal>
-          </div>
-        </div>
+        <TextField
+        sx={{
+          width: "75%",
+          "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+            height: "0.8375em",
+          },
+        }}
+          id="standingCharge"
+          name="standingCharge"
+          type="text"
+          value={formik.values.standingCharge}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.standingCharge &&
+            Boolean(formik.errors.standingCharge)
+          }
+          helperText={
+            formik.touched.standingCharge && formik.errors.standingCharge
+          }
+        />
+        <span className="text-sm text-gray-500">
+          {formik.values.currency} / month{" "}
+        </span>
       </div>
 
-      <div className="mb-6">
+      <div>
         <label
-          htmlFor="basic-rate"
-          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white "
+          htmlFor="basicTariffRate"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Price for basic tariff rate <span className="text-red-500">*</span>
         </label>
-        <div className="flex items-center">
-          <input
-            type="text"
-            id="basic-rate"
-            className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <span className="text-sm text-gray-500 ml-2">
-          {selectedCurrencySymbol} / month{" "}
-          </span>
-          <div>
-            <HelpOutlinedIcon
-              onClick={handleBasicTariffOpen}
-              className="ml-3"
-            ></HelpOutlinedIcon>
-            <Modal
-              open={basicTariffModalOpen} // Use the correct state variable
-              onClose={handleBasicTariffClose} // Use the correct close handler
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Basic tariff rate
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Enter the value of your cost for electricity consumed in basic
-                  tariff rate. If you aren't sure about it, check the costs in
-                  your last electricity invoice or in pricelist of your
-                  electricity retailer.
-                </Typography>
-              </Box>
-            </Modal>
-          </div>
-        </div>
+        <TextField
+        sx={{
+          width: "75%",
+          "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+            height: "0.8375em",
+          },
+        }}
+          id="basicTariffRate"
+          name="basicTariffRate"
+          type="text"
+          value={formik.values.basicTariffRate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.basicTariffRate &&
+            Boolean(formik.errors.basicTariffRate)
+          }
+          helperText={
+            formik.touched.basicTariffRate && formik.errors.basicTariffRate
+          }
+        />
+        <span className="text-sm text-gray-500">
+          {formik.values.currency} / month{" "}
+        </span>
       </div>
-
-      <div className="mb-6">
+      <div>
         <label
-          htmlFor="low-rate"
-          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white"
+          htmlFor="lowTariffRate"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
         >
           Price for low tariff rate
         </label>
-        <div className="flex items-center">
-          <input
-            type="text"
-            id="low-rate"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[75%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <span className="text-sm text-gray-500 ml-2">
-          {selectedCurrencySymbol} / month{" "}
-          </span>
-          <div>
-            <HelpOutlinedIcon
-              onClick={handleLowTariffOpen}
-              className="ml-3"
-            ></HelpOutlinedIcon>
-            <Modal
-              open={lowTariffModalOpen} // Use the correct state variable
-              onClose={handleLowTariffClose} // Use the correct close handler
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Low tariff rate
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Enter the value of your cost for electricity consumed in low
-                  tariff rate. If you aren't sure about it, check the costs in
-                  your last electricity invoice or in pricelist of your
-                  electricity retailer.
-                </Typography>
-              </Box>
-            </Modal>
-          </div>
-        </div>
+        <TextField
+          sx={{
+            width: "75%",
+            "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
+              height: "0.8375em",
+            },
+          }}
+          id="lowTariffRate"
+          name="lowTariffRate"
+          type="text"
+          value={formik.values.lowTariffRate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.lowTariffRate && Boolean(formik.errors.lowTariffRate)
+          }
+          helperText={
+            formik.touched.lowTariffRate && formik.errors.lowTariffRate
+          }
+        />
+        <span className="text-sm text-gray-500">
+          {formik.values.currency} / month
+        </span>
+      </div>
+      <div>
+        <label
+          htmlFor="note"
+          className="block text-xl font-medium text-gray-900 dark:text-white"
+        >
+          Note
+        </label>
+        <textarea
+          id="note"
+          rows="4"
+          name="note"
+          value={formik.values.note}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          className="block w-[75%] p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Write a note here....."
+        ></textarea>
       </div>
 
-      <label
-        htmlFor="message"
-        className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white"
-      >
-        Note
-      </label>
-
-      <textarea
-        id="message"
-        rows="4"
-        className="block p-2.5 w-[75%] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Write a note here....."
-      ></textarea>
-
-      <div className="pt-10  pr-60 pl-40 pb-40">
-        <NavLink to="/app/Price">
-          <button className="bg-transparent mt-10 w-[56%] shadow-xl float-right hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            Save Electricity Traffic
-          </button>
-        </NavLink>
+      <div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className="mt-10 float-right hover:bg-blue-500"
+          disabled={!formik.isValid}
+         
+        >
+          Save Electricity Traffic
+        </Button>
       </div>
-    </>
+    </form>
   );
 };
 
---
+++ src/features/settings/priceandrates/trafficpages/GasTariff.js
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setPageTitle } from "../../../common/headerSlice";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-70%, -70%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 10,
+  p: 4,
+};
+
+const GasTafiff = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setPageTitle({ title: "Gas Tariff" }));
+  }, []);
+
+  const [selectedCurrencySymbol, setSelectedCurrencySymbol] = useState("");
+
+  // Create separate state variables for each modal
+  const [standingChargeModalOpen, setStandingChargeModalOpen] = useState(false);
+  const [basicTariffModalOpen, setBasicTariffModalOpen] = useState(false);
+
+  const handleStandingChargeOpen = () => setStandingChargeModalOpen(true);
+  const handleStandingChargeClose = () => setStandingChargeModalOpen(false);
+
+  const handleBasicTariffOpen = () => setBasicTariffModalOpen(true);
+  const handleBasicTariffClose = () => setBasicTariffModalOpen(false);
+
+
+  // Event handler for currency selection
+  const handleCurrencyChange = (e) => {
+    const selectedValue = e.target.value;
+    console.log("Selected Currency:", selectedValue);
+    setSelectedCurrencySymbol(selectedValue);
+  };
+
+  return (
+    <>
+      <div>
+        <label
+          htmlFor="countries"
+          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white "
+        >
+          Valid from <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="date"
+          id="date"
+          className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="countries"
+          className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white "
+        >
+          Valid to
+        </label>
+        <input
+          type="date"
+          id="date"
+          className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+
+      <div>
+        <label className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white ">
+          Currency <span className="text-red-500">*</span>
+        </label>
+        <select
+          id="countries"
+          className="bg-blue-50 mb-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[75%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          onChange={handleCurrencyChange}
+          placeholder="choose a currency"
+          // defaultValue={}
+        >
+          <option defaultValue="">Choose a Currency</option>
+          <option value="₹">Indian Rupees (₹)</option>
+          <option value="$">United States Dollar ($)</option>
+          <option value="€">Euro (€)</option>
+          <option value="FC">Congolese franc (FC)</option>
+        </select>
+      </div>
+
+      <div className="mb-6">
+        <label
+          htmlFor="standing-rate"
+          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
+        >
+          Standing charge <span className="text-red-500">*</span>
+        </label>
+        <div className="flex items-center">
+          <input
+            type="text"
+            id="standing-rate"
+            className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <span className="text-sm text-gray-500 ml-2">
+            {selectedCurrencySymbol} / month{" "}
+          </span>
+          <div>
+            <HelpOutlinedIcon
+              onClick={handleStandingChargeOpen}
+              className="ml-3"
+            ></HelpOutlinedIcon>
+            <Modal
+              open={standingChargeModalOpen} // Use the correct state variable
+              onClose={handleStandingChargeClose} // Use the correct close handler
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Standing charge
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Enter the value of your monthly fees for gas. If you aren't
+                  sure about it, check the costs in your last gas invoice or in
+                  pricelist of your gas retailer.
+                </Typography>
+              </Box>
+            </Modal>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-6">
+        <label
+          htmlFor="basic-rate"
+          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white "
+        >
+          Price for consumed gas <span className="text-red-500">*</span>
+        </label>
+        <div className="flex items-center">
+          <input
+            type="text"
+            id="basic-rate"
+            className="w-[75%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <span className="text-sm text-gray-500 ml-2">
+            {selectedCurrencySymbol} / kWh{" "}
+          </span>
+          <div>
+            <HelpOutlinedIcon
+              onClick={handleBasicTariffOpen}
+              className="ml-3"
+            ></HelpOutlinedIcon>
+            <Modal
+              open={basicTariffModalOpen} // Use the correct state variable
+              onClose={handleBasicTariffClose} // Use the correct close handler
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                Consumed gas price
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Enter the value of your cost for consumed gas. If you aren't
+                  sure about it, check the costs in your last gas invoice or in
+                  pricelist of your gas retailer.
+                </Typography>
+              </Box>
+            </Modal>
+          </div>
+        </div>
+      </div>    
+
+      <label
+        htmlFor="message"
+        className="block mb-2 mt-8 text-xl font-medium text-gray-900 dark:text-white"
+      >
+        Note
+      </label>
+
+      <textarea
+        id="message"
+        rows="4"
+        className="block p-2.5 w-[75%] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Write a note here....."
+      ></textarea>
+
+      <div className="pt-10  pr-60 pl-40 pb-40">
+        <NavLink to="/app/Price">
+          <button className="bg-transparent mt-10 w-[56%] shadow-xl float-right hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Save gas Traffic
+          </button>
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default GasTafiff;
